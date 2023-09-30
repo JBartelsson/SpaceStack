ec6b6a4b74738840a69a5f51e7a05e31d38aed40
Ljpton <u34982@hs-harz.de>
2023-09-30 18:25:37 +0200

    Implemented Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool _gamePaused;
+
+    [SerializeField] private GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_gamePaused)
+            {
+                _gamePaused = false;
+                Time.timeScale = 1;
+                pauseMenu.SetActive(false);
+            }
+            else
+            {
+                _gamePaused = true;
+                Time.timeScale = 0;
+                pauseMenu.SetActive(true);
+            }
+        }
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Continue()
+    {
+        _gamePaused = false;
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+}
