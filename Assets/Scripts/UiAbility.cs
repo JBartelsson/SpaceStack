e9cd22c5c801029cca0b6197fc9b3ecfe6a33aea
JanaLuksch <jana_99@web.de>
2023-10-01 18:57:15 +0200

    Added new Icons to Canvas UI

--- Assets/Scripts/UiAbility.cs
+++ Assets/Scripts/UiAbility.cs
@@ -1,25 +1,31 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Serialization;
 using UnityEngine.UIElements;
 using static PlayerSingleton;
+
 
 public class UiAbility : MonoBehaviour
 {
 
-    [SerializeField]
-    public Sprite DashImage = null;
-    public Sprite LaserImage = null;
-    public Sprite MinimizeImage = null;
-    public Sprite GranadeImage = null;
+    [SerializeField] public Sprite dashImage;
+    [SerializeField] public Sprite laserImage;
+    [SerializeField] public Sprite minimizeImage;
+    [SerializeField] public Sprite grenadeImage;
+    [SerializeField] public Sprite circle;
+    [SerializeField] public GameObject arrow;
 
+    private RectTransform arrowTransform;
+    
     Stack<GameObject> imageStack = new Stack<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
         PlayerSingleton.Instance.OnAbilityStack += OnAbilityStackl;
+
+        arrowTransform = arrow.GetComponent<RectTransform>();
     }
 
     private void OnAbilityStackl(PlayerSingleton.Ability obj)
@@ -27,34 +33,49 @@ public class UiAbility : MonoBehaviour
         Debug.Log("Woo Event: " + obj.ToString());
         if (obj == Ability.None)
         {
-            //Pop
-            GameObject objectToDelete = imageStack.Pop();
-            Destroy(objectToDelete);
+            
+            
+            // Pop
+            Destroy(imageStack.Pop());
+            Destroy(imageStack.Pop());
+            
+            arrowTransform.anchoredPosition = new Vector2(100, -25 + (1 + imageStack.Count / 2) * 75);
+
+            if (imageStack.Count > 0)
+            {
+                arrow.SetActive(true);
+            }
+            else
+            {
+                arrow.SetActive(false);
+            }
         }
         else
         {
-            Sprite addSprite = null;
-            //push
+            Sprite addSprite;
+
+            // Push
             switch (obj)
             {
                 case Ability.Dash:
-                    addSprite = DashImage;
+                    addSprite = dashImage;
                     break;
                 case Ability.Shoot:
-                    addSprite = LaserImage;
+                    addSprite = laserImage;
                     break;
                 case Ability.Minimize:
-                    addSprite = MinimizeImage;
+                    addSprite = minimizeImage;
                     break;
                 case Ability.Grante:
-                    addSprite = GranadeImage;
+                    addSprite = grenadeImage;
                     break;
                 default:
-                    addSprite = GranadeImage;
+                    addSprite = grenadeImage;
                     break;
             }
+
+            // Symbol
             GameObject new_obj = new GameObject();
-            imageStack.Push(new_obj);
             new_obj.transform.parent = transform;
             new_obj.AddComponent<CanvasRenderer>();
             RectTransform new_rectTransform = new_obj.AddComponent<RectTransform>();
@@ -62,17 +83,34 @@ public class UiAbility : MonoBehaviour
             mImage.sprite = addSprite;
             new_rectTransform.anchorMax = new Vector2(0, 0);
             new_rectTransform.anchorMin = new Vector2(0, 0);
-            new_rectTransform.sizeDelta = new Vector2(60, 60);
-            new_rectTransform.anchoredPosition = new Vector2(50, -25 + imageStack.Count * 75);
+            new_rectTransform.sizeDelta = new Vector2(40, 40);
+            new_rectTransform.anchoredPosition = new Vector2(50, -25 + (1 + imageStack.Count / 2) * 75);
+
+            // Circle
+            GameObject circleObject = new GameObject();
+            circleObject.transform.parent = transform;
+            circleObject.AddComponent<CanvasRenderer>();
+            RectTransform newRectTransform = circleObject.AddComponent<RectTransform>();
+            UnityEngine.UI.Image symbolImage = circleObject.AddComponent<UnityEngine.UI.Image>();
+            symbolImage.sprite = circle;
+            newRectTransform.anchorMax = new Vector2(0, 0);
+            newRectTransform.anchorMin = new Vector2(0, 0);
+            newRectTransform.sizeDelta = new Vector2(60, 60);
+            newRectTransform.anchoredPosition = new Vector2(50, -25 + (1 + imageStack.Count / 2) * 75);
+
+            arrowTransform.anchoredPosition = new Vector2(100, -25 + (1 + imageStack.Count / 2) * 75);
+
+            imageStack.Push(new_obj);
+            imageStack.Push(circleObject);
             
-
-
-
+            if (imageStack.Count > 0)
+            {
+                arrow.SetActive(true);
+            }
+            else
+            {
+                arrow.SetActive(false);
+            }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
     }
 }
