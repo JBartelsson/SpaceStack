b2533592748dadb6ef8b2d912a013d31af2fc6f6
Alemay26 <135119611+Alemay26@users.noreply.github.com>
2023-10-02 00:16:07 +0200

    FMOD Update

--- Assets/Scripts/Acid/AcidDamage.cs
+++ Assets/Scripts/Acid/AcidDamage.cs
@@ -6,16 +6,12 @@ public class AcidDamage : MonoBehaviour
 {
     private void OnTriggerEnter(Collider collision)
     {
-        // Check if the game object involved in the collision has the "Player" tag
         if (collision.gameObject.CompareTag("Player"))
         {
-            // Get the PlayerSingleton component from the player game object
             PlayerSingleton playerSingleton = collision.gameObject.GetComponent<PlayerSingleton>();
 
-            Debug.Log("Collision erfolgt");
             if (playerSingleton != null)
             {
-                // Call the DamagePlayer method on the PlayerSingleton script
                 playerSingleton.DamagePlayer(100);
             }
         }
