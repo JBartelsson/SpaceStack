42a75cc167b686a228face0ca28afb668f821471
Ljpton <u34982@hs-harz.de>
2023-10-01 23:20:53 +0200

    MainMenu verknuepft

--- Assets/Scripts/MainMenuButtons.cs
+++ Assets/Scripts/MainMenuButtons.cs
@@ -7,11 +7,31 @@ public class MainMenuButtons : MonoBehaviour
 {
     public void Play()
     {
-        SceneManager.LoadScene("LevelSelection");
+        SceneManager.LoadScene("LevelSelectionNew");
+    }
+
+    public void OpenOptions()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene("Credits");
     }
 
     public void Quit()
     {
         Application.Quit();
+    }
+    
+    public void QuitOnClick()
+    {
+        Debug.Log("ButtonClick - Quit");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
     }
 }
