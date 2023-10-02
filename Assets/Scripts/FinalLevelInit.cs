c0fc178477ba58a78615e79402c7ca25c514e9c0
Johann <Joba2902@googlemail.com>
2023-10-02 12:30:08 +0200

    Fixed Last Scene and added random powerups for fun

---
+++ Assets/Scripts/FinalLevelInit.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FinalLevelInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+        for (int i = 0; i < 5; i++)
+        {
+            giveRandomPowerUp();
+        }
+        InvokeRepeating(nameof(giveRandomPowerUp), 3f, 3f);
+    }
+
+    void giveRandomPowerUp()
+    {
+        int rng = Mathf.FloorToInt(Random.Range(0, 4));
+        Debug.Log(rng);
+        if (PlayerSingleton.Instance.getAbilityStack().Count > 7) return;
+        switch (rng)
+        {
+            case 0:
+                Debug.Log("bla");
+                PlayerSingleton.Instance.pushAbilityStack(PlayerSingleton.Ability.Dash);
+                break;
+            case 1:
+                PlayerSingleton.Instance.pushAbilityStack(PlayerSingleton.Ability.Grante);
+                break;
+            case 2:
+                PlayerSingleton.Instance.pushAbilityStack(PlayerSingleton.Ability.Minimize);
+                break;
+            case 3:
+                //PlayerSingleton.Instance.pushAbilityStack(PlayerSingleton.Ability.));
+                break;
+            default:
+                break;
+        };
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
